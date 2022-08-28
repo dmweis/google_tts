7db63b1f199e22acd682ef03ee22debec0a651c4
dmweis <dmweis@users.noreply.github.com>
2022-08-28 19:57:13 +0000

    deploy: ff2a89a4544d7b31ee44ca2b03c38ef8dfcaa3af

---
+++ google_tts/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["AudioEncoding",""],["SsmlVoiceGender",""]],"mod":[["voices",""]],"struct":[["AudioConfig",""],["GoogleTtsClient",""],["ListVoicesResponse",""],["TextInput",""],["TtsResponse",""],["VoiceDescription",""],["VoiceProps",""]]};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var sourcesIndex = {};
+sourcesIndex["google_tts"] = {"name":"","files":["lib.rs","voices.rs"]};
+createSourceSidebar();
